e38987d84671ebb60a2997fc908248b42849b26c
loft475 <joseph.loftus@pnnl.gov>
2020-05-01 22:32:42 -0700

    endpoint serving up desired data

--- src/main/java/joe/loftus/great/releases/HelloController.java
+++ src/main/java/joe/loftus/great/releases/HelloController.java
@@ -39,16 +39,32 @@ public class HelloController {
 			SearchResult result = mapper.readValue(url, SearchResult.class);
 			List<Show> shows = result.getResults();
 			List<Show> highlyRated = new ArrayList<Show>();
-					
-			        for (Show show: shows) {
+			
+			for (Show show : shows) {
 
-			            if (Double.valueOf(show.getVote_average()) >= 8) {
+				if (Double.valueOf(show.getVote_average()) >= 8.5) {
 
-			                highlyRated.add(show);
-			            }
-			        }
-			        
-			        return highlyRated;
+					highlyRated.add(show);
+				}
+			}
+			
+			int pagesLeft = Integer.parseInt(result.getTotal_pages());
+			while (pagesLeft > 0) {
+				URL url2 = new URL("https://api.themoviedb.org/3/movie/now_playing?api_key=" + apiKey + "&page=" + pagesLeft);
+				SearchResult result2 = mapper.readValue(url2, SearchResult.class);
+				List<Show> shows2 = result2.getResults();
+				for (Show show2 : shows2) {
+
+					if (Double.valueOf(show2.getVote_average()) >= 8.5) {
+
+						highlyRated.add(show2);
+					}
+				}
+				pagesLeft = pagesLeft - 1;
+			}
+
+
+			return highlyRated;
 		} catch (JsonGenerationException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
@@ -58,21 +74,18 @@ public class HelloController {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * first thing - return all results with over 8.5 in is results[0].vote_average
 	 * 
-	 * To get all result -
-	 * look through all pages
-	 * to look through all pages
-	 * 	save number of pages
-	 *  make that many queries minus original
-	 *  
+	 * To get all result - look through all pages to look through all pages save
+	 * number of pages make that many queries minus original
 	 * 
-	 * in each page
-	 * look at each result and its vote average
-	 * if vote average is greater than 8.5 save show
 	 * 
-	 * soon will we pick a database that works well with our server - guessing sqlite - could practice with mongo/sql for fun
+	 * in each page look at each result and its vote average if vote average is
+	 * greater than 8.5 save show
+	 * 
+	 * soon will we pick a database that works well with our server - guessing
+	 * sqlite - could practice with mongo/sql for fun
 	 */
 }
