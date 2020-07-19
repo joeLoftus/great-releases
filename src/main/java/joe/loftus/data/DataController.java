2e0e7769999895e551877ae773603c9276625ef4
loft475 <joseph.loftus@pnnl.gov>
2020-07-18 22:10:55 -0700

    insert sql statements

--- src/main/java/joe/loftus/data/DataController.java
+++ src/main/java/joe/loftus/data/DataController.java
@@ -17,44 +17,21 @@ public class DataController {
 		Statement statement = conn.createStatement();
 		statement.execute("CREATE TABLE IF NOT EXISTS shows "
 				+ " (popularity TEXT, vote_count TEXT, video TEXT, poster_path TEXT, id TEXT, adult TEXT, backdrop_path TEXT, original_language TEXT, original_title TEXT, genre_ids TEXT[], title TEXT, vote_average TEXT, overview TEXT, release_date TEXT)");
-
-
-		
 		
 		for (int i = 0; i < shows.size(); i++) {
 			String string = "INSERT INTO shows (popularity, vote_count, video, poster_path, id, adult, backdrop_path, original_language, original_title, genre_ids, title, vote_average, overview, release_date) "
-					+ "VALUES('" + shows.get(i).getPopularity().replaceAll("'", "") + "','" + shows.get(i).getVote_count().replaceAll("'", "") + "','"
-					+ shows.get(i).getVideo().replaceAll("'", "") + "','" + shows.get(i).getPoster_path().replaceAll("'", "") + "','"
-					+ shows.get(i).getId().replaceAll("'", "") + "','" + shows.get(i).getAdult().replaceAll("'", "") + "','"
-					+ shows.get(i).getBackdrop_path().replaceAll("'", "") + "','" + shows.get(i).getOriginal_language().replaceAll("'", "") + "','"
-					+ shows.get(i).getOriginal_title().replaceAll("'", "") + "','" + shows.get(i).getGenre_ids() + "','"
-					+ shows.get(i).getTitle().replaceAll("'", "") + "','" + shows.get(i).getVote_average().replaceAll("'", "") + "','"
-					+ shows.get(i).getOverview().replaceAll("'", "") + "','" + shows.get(i).getRelease_date().replaceAll("'", "") + "')";
+					+ "VALUES('" + String.valueOf(shows.get(i).getPopularity()).replaceAll("'","") + "','"+ String.valueOf(shows.get(i).getVote_count()).replaceAll("'","") + "','"
+					+ String.valueOf(shows.get(i).getVideo()).replaceAll("'","") + "','"+ String.valueOf(shows.get(i).getPoster_path()).replaceAll("'","") + "','"
+					+ String.valueOf(shows.get(i).getId()).replaceAll("'","") + "','"+ String.valueOf(shows.get(i).getAdult()).replaceAll("'","") + "','"
+					+ String.valueOf(shows.get(i).getBackdrop_path()).replaceAll("'","") + "','"+ String.valueOf(shows.get(i).getOriginal_language()).replaceAll("'","") + "','"
+					+ String.valueOf(shows.get(i).getOriginal_title()).replaceAll("'","") + "','"+ String.valueOf(shows.get(i).getGenre_ids()) + "','"
+					+ String.valueOf(shows.get(i).getTitle()).replaceAll("'","") + "','"+ String.valueOf(shows.get(i).getVote_average()).replaceAll("'","") + "','"
+					+ String.valueOf(shows.get(i).getOverview()).replaceAll("'","") + "','"+ String.valueOf(shows.get(i).getRelease_date()).replaceAll("'","") + "')";
 			
 			System.out.println(string);
 			
 			statement.execute(string);
-			
-//			statement.execute(
-//					"INSERT INTO shows (popularity, vote_count, video, poster_path, id, adult, backdrop_path, original_language, original_title, genre_ids, title, vote_average, overview, release_date) "
-//							+ "VALUES('" + shows.get(i).getPopularity().replaceAll("'", "") + "','" + shows.get(i).getVote_count().replaceAll("'", "") + "','"
-//							+ shows.get(i).getVideo().replaceAll("'", "") + "','" + shows.get(i).getPoster_path().replaceAll("'", "") + "','"
-//							+ shows.get(i).getId().replaceAll("'", "") + "','" + shows.get(i).getAdult().replaceAll("'", "") + "','"
-//							+ shows.get(i).getBackdrop_path().replaceAll("'", "") + "','" + shows.get(i).getOriginal_language().replaceAll("'", "") + "','"
-//							+ shows.get(i).getOriginal_title().replaceAll("'", "") + "','" + shows.get(i).getGenre_ids().replaceAll("'", "") + "','"
-//							+ shows.get(i).getTitle().replaceAll("'", "") + "','" + shows.get(i).getVote_average().replaceAll("'", "") + "','"
-//							+ shows.get(i).getOverview().replaceAll("'", "") + "','" + shows.get(i).getRelease_date().replaceAll("'", "") + "')");
 		}
-
-		//
-//		statement.execute(
-//				"INSERT INTO contacts (name, phone, email) " + "VALUES('Jane', 4829484, 'jane@somewhere.com')");
-//
-//		statement.execute("INSERT INTO contacts (name, phone, email) " + "VALUES('Fido', 9038, 'dog@email.com')");
-
-//		while (results.next()) {
-//			data.add(results.getString("name") + " " + results.getInt("phone") + " " + results.getString("email"));
-//		}
 
 		statement.close();
 		conn.close();
@@ -72,7 +49,7 @@ public class DataController {
 			statement.execute("SELECT * FROM shows");
 			ResultSet results = statement.getResultSet();
 			while (results.next()) {
-				data.add(results.getString("name") + " " + results.getInt("phone") + " " + results.getString("email"));
+				data.add(results.getString("title") + " " + results.getInt("id"));
 			}
 			results.close();
 			statement.close();
