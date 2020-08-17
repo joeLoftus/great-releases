2893b0f3160a77d419da157e8cfa9f25360a48ad
loft475 <joseph.loftus@pnnl.gov>
2020-08-16 22:44:18 -0700

    cors for server

--- src/main/java/joe/loftus/greatreleases/MediaController.java
+++ src/main/java/joe/loftus/greatreleases/MediaController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 import joe.loftus.data.DataController;
 import joe.loftus.pojos.Show;
 
+@CrossOrigin
 @RestController
 public class MediaController {
 	@Autowired
 	private static final Logger logger = LoggerFactory.getLogger(ScheduledUpdater.class);
 	private DataController dataController = new DataController();
-	
+
 	@RequestMapping("/")
 	List<Show> getData() throws IOException, SQLException {
 		try {
